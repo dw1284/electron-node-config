f55a14028408b2ae271d0672fdf5aa3a83233814
Liam Whan <liam.whan@gmail.com>
2017-05-14 15:17:46 +1000

    Add guard to check with type of process the config module is called from. This prevents "Cannot read property 'getAppPath' of undefined" Errors in Renderer processes on Electron ^1.6

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 if (process.versions && process.versions.electron) {
-  process.env.NODE_CONFIG_DIR = require('electron').app.getAppPath() + '/config';
+  if(process.type === "browser") {
+      process.env.NODE_CONFIG_DIR = require('electron').app.getAppPath() + '/config';
+  } else if (process.type == "renderer") {
+      process.env.NODE_CONFIG_DIR = require('electron').remote.app.getAppPath() + '/config';
+  }
 }
 
 module.exports = require('config');
